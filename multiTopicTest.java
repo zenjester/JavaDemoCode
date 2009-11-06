6bb01b41817cc2b8453b01472dc45cc125e5fef9
zenjester <zenjester@gmail.com>
2009-11-06 22:40:06 +0000

    test advent updatetest advent updatetest advent updatetest advent updatetest advent updatetest advent updatetest advent updatetest advent updatetest advent updatetest advent updatetest advent update

--- multiTopicTest.java
+++ multiTopicTest.java
@@ -2,9 +2,12 @@ public class multiTopicTest {
   public static void main (String args[])
   {
     multiTopic mt1= new multiTopic();
-    
+
+//create two new objects    
     mt1.setTopic("Plastering");
     mt1.setTopicLength(5);
+
+//print out the relevant attributes
     
     System.out.println(" course name :" + mt1.getTopic());
     System.out.println(" course is " + mt1.getTopicLength() + " weeks long");
