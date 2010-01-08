4c33ebe6b19b5379250a35aab30473f96939f529
zenjester <zenjester@gmail.com>
2010-01-08 22:01:23 +0000

    post latvia 090110

---
+++ TwoButtons.java
@@ -0,0 +1,57 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.Graphics;
+
+public class TwoButtons {
+
+	JFrame frame;
+	JLabel label;
+	
+	public static void main (String [] args) {
+		TwoButtons gui = new TwoButtons();
+		gui.go();
+
+
+	}
+	
+	public void go() {
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton labelButton = new JButton("Change Label");
+		labelButton.addActionListener(new LabelListener());
+		
+		JButton colorButton = new JButton("Change Circle");
+		colorButton.addActionListener(new ColorListener());
+		
+		label = new JLabel("I'm a label ");
+		MyDrawPanel drawPanel = new MyDrawPanel();
+		
+		frame.getContentPane().add(BorderLayout.SOUTH, colorButton);
+		frame.getContentPane().add(BorderLayout.CENTER, drawPanel);
+		frame.getContentPane().add(BorderLayout.EAST, labelButton);
+		frame.getContentPane().add(BorderLayout.WEST, label);
+		
+		frame.setSize(300,300);
+		frame.setVisible(true);
+	}
+		
+		class LabelListener implements ActionListener {
+			public void actionPerformed(ActionEvent event) {
+				label.setText("Ouch");
+			}
+		}
+		
+		class ColorListener implements ActionListener{
+			public void actionPerformed(ActionEvent event) {
+				frame.repaint();
+			}
+		}
+		// this dos not work I think Ihave missed an import
+		class MyDrawPanel extends JPanel  {
+			public void paintComponent (Graphics g) {
+				g.setColor(Color.orange);
+				g.fillOval(10,10,100,100);
+			}
+		}
+	}
